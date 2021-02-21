6e459d33b21089cf91ba5c27db575c1ac337a7c0
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2021-02-21 17:16:54 -0300

    Final aula: Revisão introdutória

--- PrimeirosPassosdotNET/Revisao/Program.cs
+++ PrimeirosPassosdotNET/Revisao/Program.cs
@@ -6,7 +6,32 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        // TODO: adicionar aluno
+                        break;
+                    case "2":
+                        // TODO: listar alunos
+                        break;
+                    case "3":
+                        // TODO: calcular média geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
         }
     }
 }
