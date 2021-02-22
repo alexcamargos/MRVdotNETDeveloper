96ad2037a2bd1a5efca1af446e5031d8b04d4b4b
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2021-02-22 06:58:41 -0300

    Final aula: Praticando primeiros passos

--- PrimeirosPassosdotNET/Revisao/Program.cs
+++ PrimeirosPassosdotNET/Revisao/Program.cs
@@ -6,6 +6,72 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
+
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o nome do aluno: ");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno: ");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("O valor da nota deve ser decimal.");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
+                        break;                       
+                    case "2":
+                        foreach (var _aluno in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(_aluno.Nome))
+                            {
+                                Console.WriteLine($"ALUNO: {_aluno.Nome} - NOTA: {_aluno.Nota}");
+                            }
+                        }
+
+                        break;
+                    case "3":
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+
+                        for (int i = 0; i < alunos.Length; i++)
+                        {
+                            if (!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notaTotal += alunos[i].Nota;
+                                nrAlunos++;
+                            }
+                        }
+
+                        var mediaGeral = notaTotal / nrAlunos;
+                        Console.WriteLine($"MÉDIA GERAL: {mediaGeral}");
+
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
             Console.WriteLine("Informe a opção desejada:");
             Console.WriteLine("1 - Inserir novo aluno");
             Console.WriteLine("2 - Listar alunos");
@@ -14,24 +80,8 @@ namespace Revisao
             Console.WriteLine();
 
             string opcaoUsuario = Console.ReadLine();
-
-            while (opcaoUsuario.ToUpper() != "X")
-            {
-                switch (opcaoUsuario)
-                {
-                    case "1":
-                        // TODO: adicionar aluno
-                        break;
-                    case "2":
-                        // TODO: listar alunos
-                        break;
-                    case "3":
-                        // TODO: calcular média geral
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
